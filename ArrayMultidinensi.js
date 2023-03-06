e871c6c2f0575120678464c381d2fe2f0ecbc79a
khoerulfajri1999 <113015258+khoerulfajri1999@users.noreply.github.com>
2023-03-06 12:59:19 +0700

    Membuat file tugas javascript Array Multidimensi

---
+++ ArrayMultidinensi.js
@@ -0,0 +1,26 @@
+function multidimensi_array() {
+  var arr = [];
+  var abjad="abcdefghi"
+  var counter=0;
+  for (var i = 0; i < 3; i++) {
+    // row
+    var row = [];
+    for (var j = 0; j < 3; j++) {
+      // col
+          row.push(abjad[counter]);
+          counter++;
+      
+      // ['x']
+      // ['x','x']
+    }
+
+    arr.push(row);
+    arr.push("\n");
+  }
+
+  return arr;
+}
+
+
+
+console.log(multidimensi_array());
